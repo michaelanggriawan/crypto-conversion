05145fcea589032c393fca4db461addeaf918986
michaelanggriawan <michaelanggriawan941@gmail.com>
2024-03-25 01:30:05 +0700

    feat: change input type text to number

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     appDir: true,
   },
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL ?? 'http://localhost:3001/v1/api/'
   },
   output: 'standalone',
   experimental: {
